ce0cc5a8f26e2d06e68c42428f093a608126859f
Adam Montano <amontano495@gmail.com>
2024-10-01 12:28:29 -0700

    added leetcode.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,5 +22,23 @@ return {
     --add tmux navigator
     {
         "christoomey/vim-tmux-navigator",
+    },
+    --add leetcode.nvim
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim", -- required by telescope
+            "MunifTanjim/nui.nvim",
+
+            -- optional
+            "nvim-treesitter/nvim-treesitter",
+            "rcarriga/nvim-notify",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            -- configuration goes here
+        },
     }
 }
